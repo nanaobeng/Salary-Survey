2d0f1e863a300ce793e7325f145ce23126c0923a
nanaobeng <nanaobengmarnu@gmail.com>
2020-10-20 17:59:58 +0000

    fdfd

--- survey/main/routes.py
+++ survey/main/routes.py
@@ -17,8 +17,12 @@ def index():
 
 @main.route("/")
 def landing():
-    if current_user.is_authenticated:
+    if current_user.is_authenticated and current_user.role == 'admin':
+        
+        return redirect(url_for('users.admin_home'))
+    if current_user.is_authenticated and current_user.role != 'admin':
         return redirect(url_for('main.index'))
+
     return render_template("landing.html")
 
 
