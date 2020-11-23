e9da6ba71721d42acd8297b5e175f8fc92362d57
nanaobeng <nanaobengmarnu@gmail.com>
2020-11-23 21:36:19 +0000

    models

--- survey/models.py
+++ survey/models.py
@@ -21,7 +21,7 @@ class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True)
     email = db.Column(db.String(120), unique=True)
-    role = db.Column(db.String(120),  nullable=False)
+    role = db.Column(db.String(120))
     password = db.Column(db.String(60),  nullable=False)
     client = db.relationship('Client', backref='client' , lazy=True)
     audit_log = db.relationship('Audit_log', backref='log' , lazy=True)
